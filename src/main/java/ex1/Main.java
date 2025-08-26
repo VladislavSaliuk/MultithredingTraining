187a18f7e5f1efbd1acead543fa05e1330ee6184
VladislavSaliuk <salukvladislav81@gmail.com>
2025-08-26 09:00:31 +0300

    Implemented Custom Threads with Thread class

---
+++ src/main/java/ex1/Main.java
@@ -0,0 +1,16 @@
+package ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread1 = new MyThread();
+        thread1.setName("MyThread-1");
+        Thread thread2 = new MyThread();
+        thread2.setName("MyThread-2");
+        Thread thread3 = new MyThread();
+        thread3.setName("MyThread-3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
